5cd743f9373290b5f3689473d4326585d15193d4
Eyal Engel <eyalengel13@gmail.com>
2025-04-08 02:06:22 +0300

    add empty route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import StoreOwnerOrders from "./pages/StoreOwnerOrders";
 import SupplierAuth from "./pages/SupplierAuth";
 import SupplierOrders from "./pages/SupplierOrders";
+import StoreOwnerOrders from "./pages/StoreOwnerOrders";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/supplier" />} />
       <Route path="/store-owner/orders" element={<StoreOwnerOrders />} />
       <Route path="/supplier" element={<SupplierAuth />} />
       <Route path="/supplier/orders" element={<SupplierOrders />} />
